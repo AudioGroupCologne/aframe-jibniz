66573a7c4d37b08e962e515f8871bba275042c50
Damian-T-D <damian.dziwis@th-koeln.de>
2023-03-23 16:50:59 +0100

    Update aframe-jibniz.js

--- aframe-jibniz.js
+++ aframe-jibniz.js
@@ -236,3 +236,28 @@ AFRAME.registerComponent("canvas-updater", {
     material.displacementMap.needsUpdate = true;
   },
 });
+
+/* global AFRAME, NAF */
+AFRAME.registerComponent('ibniz-persistent', {
+  schema: {
+    template: { default: '' },
+    keyCode: { default: 32 }
+  },
+
+  init: function() {
+    this.onKeyUp = this.onKeyUp.bind(this);
+    document.addEventListener("keyup", this.onKeyUp);
+  },
+
+  onKeyUp: function(e) {
+    if (this.data.keyCode === e.keyCode || e.keyCode == undefined) {
+      const el = document.createElement('a-entity');
+      this.el.sceneEl.appendChild(el);
+      el.setAttribute('position', this.el.getAttribute('position'));
+      el.setAttribute('networked', {persistent: true, template: this.data.template});
+      NAF.utils.getNetworkedEntity(el).then((networkedEl) => {
+        document.body.dispatchEvent(new CustomEvent('persistentEntityCreated', {detail: {el: el}}));
+      });
+    }
+  }
+});
